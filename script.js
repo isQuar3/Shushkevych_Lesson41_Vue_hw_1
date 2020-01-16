a0fcf0d8a8937934236ef57d94c5a7af34645ea6
isQuar3 <isquaremain@gmail.com>
2020-01-16 18:27:46 +0300

    ready hw 1

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    var changeColor = new Vue({
+        el: "#app",
+        data: {
+            userColor: '',
+            color: '',
+            backupColor: '',
+        },
+        methods: {
+            changecolor: function () {
+                this.color = this.userColor;
+                document.getElementById("main-block").style.backgroundColor = this.color;
+                document.getElementById("result").innerText = `Ваш цвет фона теперь "${this.color.toUpperCase()}"`;
+                document.getElementById("wrapper").style.backgroundColor = "white";
+                this.userColor = '';
+            }
+        }
+    });
+};
